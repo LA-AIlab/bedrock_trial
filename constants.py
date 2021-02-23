c3aaa907e9b1c67203c7cee9f1becd5c08173901
w-hajra <39582218+w-hajra@users.noreply.github.com>
2021-02-23 12:19:28 +0500

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+FEATURE_COLS=["Q1+Q2+Q3+Q4 DataPlus", "Q1+Q2+Q3+Q4 DualPane",
+       "Q1+Q2+Q3+Q4 ExternalQuiz", "Q1+Q2+Q3+Q4 Folder", "Q1+Q2+Q3+Q4 Forumng",
+       "Q1+Q2+Q3+Q4 Glossary", "Q1+Q2+Q3+Q4 HomePage",
+       "Q1+Q2+Q3+Q4 HtmlActivity", "Q1+Q2+Q3+Q4 Oucollaborate",
+       "Q1+Q2+Q3+Q4 OuContent", "Q1+Q2+Q3+Q4 OuElluminate",
+       "Q1+Q2+Q3+Q4 Ouwiki", "Q1+Q2+Q3+Q4 Page", "Q1+Q2+Q3+Q4 Questionnaire",
+       "Q1+Q2+Q3+Q4 Quiz", "Q1+Q2+Q3+Q4 RepeatActivity",
+       "Q1+Q2+Q3+Q4 Resource", "Q1+Q2+Q3+Q4 SharedSubpage",
+       "Q1+Q2+Q3+Q4 Subpage", "Q1+Q2+Q3+Q4 Url"]
+
+ARGET_COL = "final_result"
+
