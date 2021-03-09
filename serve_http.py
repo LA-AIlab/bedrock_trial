73037ab82cba0a25d11525e103a3b08e31fd5055
w-hajra <39582218+w-hajra@users.noreply.github.com>
2021-03-09 14:26:57 +0500

    Update serve_http.py

--- serve_http.py
+++ serve_http.py
@@ -11,7 +11,7 @@ import numpy as np
 from bedrock_client.bedrock.metrics.service import ModelMonitoringService
 from flask import Flask, Response, current_app, request
 # from trainn import X_val,y_val
-from train_bedrock import data
+# from train_bedrock import data
 from constants import FEATURE_COLS
 
 OUTPUT_MODEL_NAME = "/artefact/lgb_model.pkl"
